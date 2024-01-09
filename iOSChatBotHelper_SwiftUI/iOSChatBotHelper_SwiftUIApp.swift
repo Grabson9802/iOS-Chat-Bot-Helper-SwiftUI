468fb17c903dcfab6ad4a26bfd4e9bffdf2b64a9
Krystian Grabowy <krys9802@gmail.com>
2024-01-09 17:26:24 +0100

    Initial Commit

---
+++ iOSChatBotHelper_SwiftUI/iOSChatBotHelper_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSChatBotHelper_SwiftUIApp.swift
+//  iOSChatBotHelper_SwiftUI
+//
+//  Created by Krystian Grabowy on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSChatBotHelper_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
